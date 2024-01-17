b16e6cf6109ef41eb85fdfe720ca11cb2edc74f9
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-01-17 13:07:31 +0100

    feat: switch to replexica cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -l
 
-echo "Hello, Replexica"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+# Run Replexica CLI to localize missing strings
+npx replexica@latest localize
+# Commit the changes into the current branch
+git config --global user.name "Replexica"
+git config --global user.email "support@replexica.com"
+git add .
+if git diff --staged --quiet; then
+  echo "::notice::Replexica has not found any missing translations"
+else
+  git commit -m "feat: add missing translations [skip ci]"
+  git push
+  # retrieve the last commit hash
+  COMMIT_HASH=$(git rev-parse HEAD)
+  # build a github url to the commit
+  COMMIT_URL="https://github.com/replexica/replexica/commit/$COMMIT_HASH"
+  # output the commit url to the github actions announcements using ::notice::
+  echo "::notice::Replexica has just added missing translations in the following commit: $COMMIT_URL"
+fi
